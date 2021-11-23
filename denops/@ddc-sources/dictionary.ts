c170be8c26a1818aba6a3d2ed7020b83b739f125
matsui54 <haru.matu9168@gmail.com>
2021-11-23 15:11:32 +0900

    Update deps

--- denops/@ddc-sources/dictionary.ts
+++ denops/@ddc-sources/dictionary.ts
@@ -2,12 +2,12 @@ import {
   BaseSource,
   Candidate,
   DdcEvent,
-} from "https://deno.land/x/ddc_vim@v0.17.0/types.ts#^";
+} from "https://deno.land/x/ddc_vim@v0.18.0/types.ts#^";
 import {
   GatherCandidatesArguments,
   OnEventArguments,
-} from "https://deno.land/x/ddc_vim@v0.17.0/base/source.ts#^";
-import { fn } from "https://deno.land/x/ddc_vim@v0.17.0/deps.ts#^";
+} from "https://deno.land/x/ddc_vim@v0.18.0/base/source.ts#^";
+import { fn } from "https://deno.land/x/ddc_vim@v0.18.0/deps.ts#^";
 
 type DictCache = {
   mtime: Date | null;
