7b3b3dbf0d40768f3032e3e2a52f5ead1f8d4608
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-11-10 16:16:35 +0900

    Update deps

--- denops/@ddc-sources/dictionary.ts
+++ denops/@ddc-sources/dictionary.ts
@@ -2,13 +2,13 @@ import {
   BaseSource,
   DdcEvent,
   Item,
-} from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
+} from "https://deno.land/x/ddc_vim@v4.1.0/types.ts";
 import {
   GatherArguments,
   OnEventArguments,
-} from "https://deno.land/x/ddc_vim@v3.4.0/base/source.ts";
-import { fn } from "https://deno.land/x/ddc_vim@v3.4.0/deps.ts";
-import { assertEquals } from "https://deno.land/std@0.187.0/testing/asserts.ts";
+} from "https://deno.land/x/ddc_vim@v4.1.0/base/source.ts";
+import { fn } from "https://deno.land/x/ddc_vim@v4.1.0/deps.ts";
+import { assertEquals } from "https://deno.land/std@0.205.0/testing/asserts.ts";
 
 type DictCache = {
   mtime: Date | null;
